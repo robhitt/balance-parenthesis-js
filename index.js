154b7f58fb08e0c41646e104c4844ef07d322441
Matthew Curtis <matthew.curtis@darkowl.com>
2019-09-09 17:49:41 -0500

    Clean up logic

--- index.js
+++ index.js
@@ -3,48 +3,28 @@
 // *********************************
 // USE A STACK!!!
 
-var tokens = [ ['{','}'] , ['[',']'] , ['(',')'] ];
+var tokens = {
+  '[': ']',
+  '{': '}',
+  '(': ')'
+};
+var opening = Object.keys(tokens);
+var closing = Object.values(tokens);
 
 var btn = document.getElementById('btn');
-btn.addEventListener('click', isBalanced);
+btn.addEventListener('click', checkIsBalanced);
 
 document.getElementById("input-one").addEventListener("keyup", function(event) {
     event.preventDefault(); // Left this in case I convert to a form in the future
     if (event.keyCode == 13) {
-        var parensStr = document.getElementById('input-one');
-        var inputStr = parensStr.value
-        isBalanced(inputStr);
+      checkIsBalanced()
     }
 });
 
-// *** Check if character is an opening bracket ***
-function isOpenParenthesis(parenthesisChar) {
-  for (var j = 0; j < tokens.length; j++) {
-    if (tokens[j][0] === parenthesisChar) {
-      return true;
-    }
-  }
-  return false;
-}
-
-// *** Check if opening bracket matches closing bracket ***
-function matches(topOfStack, closedParenthesis) {
-  for (var k = 0; k < tokens.length; k++) {
-    if (tokens[k][0] === topOfStack && tokens[k][1] === closedParenthesis) {
-      return true;
-    }
-  }
-  return false;
-}
-
-// *** Checks if item is any sort of paranthesis ***
-function isParanthesis(char) {
-  var str = '{}[]()';
-  if (str.indexOf(char) > -1) {
-    return true;
-  } else {
-    return false;
-  }
+function checkIsBalanced() {
+  var parensStr = document.getElementById('input-one');
+  var inputStr = parensStr.value
+  printToScreen(isBalanced(inputStr));
 }
 
 // *** Prints answer of the string to the HTML page  ***
@@ -59,29 +39,44 @@ function printToScreen(bool) {
   }
 }
 
+// *** Check if character is an opening bracket ***
+function isOpenParentheses(parenthesesChar) {
+  return opening.includes(parenthesesChar)
+}
+
+// *** Check if character is an closing bracket ***
+function isCloseParentheses(parenthesesChar) {
+  return closing.includes(parenthesesChar)
+}
+
+// *** Check if opening bracket matches closing bracket ***
+function matches(topOfStack, closedParentheses) {
+  return tokens[topOfStack] === closedParentheses
+}
+
 // *** We excute this function upon the event ***
-function isBalancedinputStr {
+function isBalanced(inputStr) {
   if (inputStr === null) { return true; }
 
   var expression = inputStr.split('');
   var stack = [];
 
+  var currChar, top
   for (var i = 0; i < expression.length; i++) {
-    if (isParanthesis(expression[i])) {
-      if (isOpenParenthesis(expression[i])) {
-        stack.push(expression[i]);
-      } else {
-        if (stack.length === 0) {
-          return printToScreen(false);
-        }
-        var top = stack.pop(); // pop off the top element from stack
-        if (!matches(top, expression[i])) {
-          return printToScreen(false);
-        }
+    currChar = expression[i]
+    if (isOpenParentheses(currChar)) {
+      stack.push(currChar);
+    } else if (isCloseParentheses(currChar)) {
+      if (stack.length === 0) {
+        return false;
+      }
+
+      top = stack.pop(); // pop off the top element from stack
+      if (!matches(top, currChar)) {
+        return false;
       }
     }
   }
 
-  var returnValue = stack.length === 0 ? true : false;
-  printToScreen(returnValue)
+  return !stack.length;
 }
